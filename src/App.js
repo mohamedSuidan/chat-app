ceca3c171504158acd2fc2f7766a957db9297e6f
mohamedSuidan <msuidan149@gmail.com>
2023-06-11 14:58:03 +0300

    v-2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import Chating from "./Chating";
 function App() {
   const socket = io.connect("http://localhost:4000"); // backend server link
   const user = JSON.parse(localStorage.getItem("user"));
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [users, setUsers] = useState([]);
-
+  const [userId, setUserId] = useState("");
+  const [name, setName] = useState("");
   useEffect(() => {
     const fetchData = async () => {
       let usersData = await axios.get("http://localhost:4000/get-users");
@@ -26,28 +28,49 @@ function App() {
       socket.disconnect();
     };
   }, []);
-
+  const startChat = (e) => {
+    e.preventDefault();
+    setUserId(e.target.id);
+    setName(e.target.dataset.name);
+  };
   return (
     <div className="chat">
       <div className="row">
         <div className="col-1 border">
           {users.users
-            ? onlineUsers.map((ele, i) => {
+            ? users.users.map((ele, i) => {
                 return (
                   <div className="row color" key={i}>
                     <div className="col-img">
                       <img src="https://www.w3schools.com/w3images/avatar2.png" />
                     </div>
                     <div className="col-text">
-                      <Link to={`/chat/${ele.user_id}`}>
+                      <Link
+                        to={`/chat/${ele._id}`}
+                        onClick={startChat}
+                        id={ele._id}
+                        data-name={ele.name}
+                      >
                         {ele.name === user.name ? "me" : ele.name}
                       </Link>
-                      <p className="online">online</p>
+                      {/* <p className="online">online</p> */}
                     </div>
                   </div>
                 );
               })
             : ""}
+        </div>
+        <div className="col-2">
+          {userId !== "" ? (
+            <Chating
+              userId_recever={userId}
+              socket={socket}
+              user={user}
+              name={name}
+            />
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </div>
